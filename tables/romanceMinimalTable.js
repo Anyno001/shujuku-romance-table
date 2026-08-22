4194d098747f3b0a8725a73b540dbc8664248990
仓库 <yyyeven@gmail.com>
2026-08-23 00:11:29 +0800

    发布 v3.3 拆分资源 tables/romanceMinimalTable.js

---
+++ tables/romanceMinimalTable.js
@@ -0,0 +1,40 @@
+export const romanceMinimalTable = {
+  "id": "romance-minimal-table",
+  "title": "最小 shujuku 表格模板",
+  "databaseKey": "romance-minimal-table",
+  "template": {
+    "mate": {
+      "type": "chatSheets",
+      "version": 1,
+      "name": "最小 shujuku 表格模板"
+    },
+    "sheet_0": {
+      "uid": "sheet_0",
+      "name": "角色状态",
+      "content": [
+        [
+          "属性",
+          "值",
+          "备注"
+        ],
+        [
+          "生命值",
+          "100",
+          "初始值"
+        ],
+        [
+          "金币",
+          "0",
+          "初始值"
+        ]
+      ],
+      "sourceData": {
+        "headers": [
+          "属性",
+          "值",
+          "备注"
+        ]
+      }
+    }
+  }
+};
